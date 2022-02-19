94d5678bf1b7125bb1a76c73f6711a070f023dab
Vladimir Levadny <v.levadny@rambler.ru>
2022-02-19 06:08:44 +0300

    add hooks and types for actions

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {store} from './store/index'
+import {Provider} from "react-redux";
+
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
   document.getElementById('root')
 );
